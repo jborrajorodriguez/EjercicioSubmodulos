f6f2f97cf9914a949e9971f0189967589558aaec
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-01-26 09:55:47 +0100

    Libreria añadida

--- src/ejerciciosubmodulos/EjercicioSubmodulos.java
+++ src/ejerciciosubmodulos/EjercicioSubmodulos.java
@@ -1,10 +1,14 @@
 package ejerciciosubmodulos;
+import com.juan.datos.PedirDatos;
+        
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class EjercicioSubmodulos {
 
     public static void main(String[] args) {
+        PedirDatos.leerString("Hola");
+        
 
     }
 
